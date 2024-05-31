ce23a7824a43aac0f284e3d9afa84b2a16d9f916
gyeong <gyeongyeongi22@gmail.com>
2024-05-31 19:11:00 +0900

    [PULL] 풀 전 커밋

--- app/src/main/java/com/corporation8793/dementia/MainActivity.java
+++ app/src/main/java/com/corporation8793/dementia/MainActivity.java
@@ -4,13 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.opencsv.exceptions.CsvException;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button seoul, daegu, gwangju;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            Region.loadData(MainActivity.this);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (CsvException e) {
+            throw new RuntimeException(e);
+        }
+
+        seoul = findViewById(R.id.seoul);
+        daegu = findViewById(R.id.daegu);
+        gwangju = findViewById(R.id.gwangju);
+
+        Region region = new Region();
+
+        seoul.setOnClickListener(v->{
+            region.check_region("서울특별시");
+        });
+
+        daegu.setOnClickListener(v->{
+            region.check_region("대구광역시");
+        });
+
+
+        gwangju.setOnClickListener(v->{
+            region.check_region("경기도");
+        });
+
+
 
     }
 }
